8c0e163ba39991b12f15659f9b99a573d5321526
eecheverri <ez.echeverri@gmail.com>
2019-03-30 23:19:09 -0300

    Update DtGato.cpp
    Prueba1 Eze

--- 5_lab0/DtGato.cpp
+++ 5_lab0/DtGato.cpp
@@ -11,4 +11,5 @@ DtGato::DtGato () {
 
 DtGato::DtGato (string nombre,Genero genero,float peso,float racionDiaria,TipoPelo tipoPelo):DtMascota(nombre,genero,peso,racionDiaria){
 	this->tipoPelo = tipoPelo;
+	//prueba git
 }
