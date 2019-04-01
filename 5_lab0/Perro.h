a0a9aca5b2c18ac2cca17b502af3c0d0779b4c61
Fede1992 <49131245+Fede1992@users.noreply.github.com>
2019-04-01 10:30:44 -0300

    Create Perro.h

---
+++ 5_lab0/Perro.h
@@ -0,0 +1,28 @@
+#ifndef Perro
+#define Perro
+
+class Perro: pubic Mascota
+{
+  private: RazaPerro raza;
+           bool vacunaCachorro;
+           
+  public:
+          //constructor por defecto
+          Perro();
+          //constructor comun
+          Perro(RazaPerro praza, bool pvacunaCachorro):base(pnombre,pgenero,ppeso);
+          
+          //propiedades
+          void setRazaPerro();
+          RazaPerro getRazaPerro();
+          
+          void setVacunaCachorro();
+          bool vacunaCachorro();
+          
+          //operaciones
+          float obtenerRacionDiaria();
+          
+          //Destructor
+          Perro::~Perro(){}
+          
+}
